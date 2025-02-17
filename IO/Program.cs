0bdda900b99d3f37c81570044860513f043636f5
ilyat <ilya.tolo@mail.ru>
2025-02-18 00:22:15 +0700

    Добавлены интерфейсы ввода и вывода с place holder'ами для будущий функций

--- IO/Program.cs
+++ IO/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+interface IOutput {
+   public void Export(); // функция, берущая класс с фигурами
+}
+
+interface IInput {
+   public void Import();// функция, возвращающая класс с фигурами
+}
