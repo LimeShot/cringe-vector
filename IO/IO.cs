ccd1ec4281110169213c2264b411aeca990bfa2f
ilyat <ilya.tolo@mail.ru>
2025-02-18 21:42:17 +0700

    Добавлена возможность создавать файл .svg (расширение файла зависит от пути, переданного в йункцию), причём если файл существует, то его перезаписывает, а если нет, то он создаётся.

---
+++ IO/IO.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace IO.Method;
+interface IOutput {
+   public void Export(); // функция, берущая класс с фигурами
+}
+
+interface IInput {
+   public void Import(); // функция, возвращающая класс с фигурами
+}
+
+public class OutputToSVG : IOutput {
+   public string Path;
+   public OutputToSVG(string path) {
+      Path = path;
+   }
+   public void Export() {
+      using (FileStream fstream = new FileStream(Path, FileMode.OpenOrCreate)) {
+         string text = "<svg>\n<\\svg>";
+         if (!File.Exists(Path)) {
+            File.Create(Path);
+         }
+         // преобразуем строку в байты
+         byte[] buffer = Encoding.Default.GetBytes(text);
+         // запись массива байтов в файл
+         fstream.WriteAsync(buffer, 0, buffer.Length);
+         Console.WriteLine("Текст записан в файл");
+      }
+   }
+}
--- IO/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-interface IOutput {
-   public void Export(); // функция, берущая класс с фигурами
-}
-
-interface IInput {
-   public void Import();// функция, возвращающая класс с фигурами
-}
