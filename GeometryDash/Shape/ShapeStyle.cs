3ad2f9cb90adb15fe25ab79088784924c4bb1495
Grisha2020 <grimeh@mail.ru>
2025-02-21 00:09:51 +0700

    Абстрактный класс переделан на интерфейс

---
+++ GeometryDash/Shape/ShapeStyle.cs
@@ -0,0 +1,24 @@
+using OpenTK.Mathematics;
+
+namespace CringeCraft.GeometryDash.Shape;
+
+public class ShapeStyle {
+    public Vector3 ColorOutline { set; get; }
+    public Vector3 ColorFill { set; get; }
+    public bool Fill { set; get; }
+    public bool Visible { set; get; }
+
+    public ShapeStyle() {
+        ColorOutline = (0.0f, 0.0f, 0.0f);
+        ColorFill = (0.0f, 0.0f, 0.0f);
+        Fill = true;
+        Visible = true;
+    }
+
+    public ShapeStyle(Vector3 colorOutline, Vector3 colorFill, bool fill, bool visible) {
+        ColorOutline = colorOutline;
+        ColorFill = colorFill;
+        Fill = fill;
+        Visible = visible;
+    }
+}
