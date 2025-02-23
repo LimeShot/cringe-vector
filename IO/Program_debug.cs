f69b398a3e4c1550a642a0d05564d09f90912a7a
DmitryBog <20pavlov12@gmail.com>
2025-02-23 22:32:28 +0700

    Добавлены кейсы для анализа холста

--- IO/Program_debug.cs
+++ IO/Program_debug.cs
@@ -4,7 +4,7 @@ using IO.Method;
 
 class Program {
     static void Main(string[] args) {
-        var Tmp = new OutputToSVG(@"C:\myfiles\note.svg");
+        var Tmp = new OutputToSVG(@"D:\baZa\note.svg");
         Tmp.Export();
         Console.WriteLine("Something happened");
     }
