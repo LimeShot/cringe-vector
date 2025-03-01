4059a0951cc4d745ec9e055ce7505ff8453efc31
Grisha2020 <grimeh@mail.ru>
2025-03-02 01:39:35 +0700

    Перенесен ICanvas в папку GeometryDash

---
+++ GeometryDash/ICanvas.cs
@@ -0,0 +1,11 @@
+namespace CringeCraft.GeometryDash;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using CringeCraft.GeometryDash.Shape;
+
+public interface ICanvas : INotifyPropertyChanged {
+    public ObservableCollection<IShape> Shapes { get; set; }
+    public float Width { get; set; }
+    public float Height { get; set; }
+}
