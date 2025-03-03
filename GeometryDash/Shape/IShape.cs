f0b51ddf1f4cf3832c865a5d42488171db0cc199
Grisha2020 <grimeh@mail.ru>
2025-03-03 20:46:04 +0700

    Изменен интерфейс IShape

--- GeometryDash/Shape/IShape.cs
+++ GeometryDash/Shape/IShape.cs
@@ -4,22 +4,40 @@ using OpenTK.Mathematics;
 
 using System.ComponentModel;
 
-public interface IShape : INotifyPropertyChanged {
-    public Vector2 Translate { set; get; }
-    public float Rotate { set; get; }
+public interface IShape {
+    public Vector2 Translate { get; }
+    public float Z { set; get; }
+    public float Rotate { get; }
     public ShapeStyle Style { set; get; }
+    public Vector2[] BoundingBox { get; }
+    public Vector2[] Nodes { set; get; }
 
-    //Метод возвращает линии фигуры(без заполнения(Fill=false)), необходимые для отрисовки
+    //Метод возвращает линии фигуры(без заполнения(Fill=false)) необходимые для отрисовки
     public float[] GetLineVertices();
 
-    //Метод возвращает треугольники(с заполнением(Fill=true)), необходимые для отрисовки фигуры
+    //Метод возвращает треугольники(с заполнением(Fill=true)) необходимые для отрисовки фигуры
     public float[] GetTriangleVertices();
 
-    public Vector2[] GetBoundingBox();
+    //Метод возвращает окружность(с заполнением(Fill=false)) необходимые для отрисовки фигуры
+    public float[] GetCircumferenceVertices();
+
+    //Метод возвращает Эллипс(с заполнением(Fill=true)) необходимые для отрисовки фигуры
+    public float[] GetCircleVertices();
 
     //Метод для проверки принадлежности точки фигуре 
-    public bool ContainsPoint(Vector2 point);
+    public bool IsBelongsShape(Vector2 point);
+
+    //Метод для проверки принадлежности точки BoundingBox, возвращает индекс вершины
+    public int IsBBNode(Vector2 point);
 
     // Метод для перемещения фигуры на основе разницы между двумя точками
-    public void Move(float x1, float y1, float x2, float y2);
+    public void Move(Vector2 oldPoint, Vector2 newPoint);
+
+    // Метод чисто для Безье(но это не точно)
+    public void MoveNode(int index, Vector2 newNode);
+
+    // Метод изменения фигуры при изменении координат только одного узла BoundingBox
+    public void Resize(int index, Vector2 newNode);
+
+    public event Action? OnChange;
 }
