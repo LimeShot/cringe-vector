99053cc9b261ab992b69af6dd523629d802129a6
timber <korovasibiri@gmail.com>
2025-03-04 19:23:05 +0700

    Добавлен JSON конвертер

--- IO/IO.cs
+++ IO/IO.cs
@@ -1,29 +1,39 @@
-﻿namespace CringeCraft.IO;
-
-using System.Text;
-
+﻿using System.Text;
+using System.Text.Json;
 using CringeCraft.GeometryDash;
 
+namespace CringeCraft.IO;
 interface IOutput {
-    public void Export(ICanvas field); // функция, берущая класс с фигурами
+    public void Export(ICanvas Field); // функция, берущая класс с фигурами
 }
 
 interface IInput {
     public void Import(); // функция, возвращающая класс с фигурами
 }
 
-public class OutputToSVG : IOutput {
+public class OutputToCRNG : IOutput {
     public string Path;
-
-    public OutputToSVG(string path) {
+    public OutputToCRNG(string path) {
         Path = path;
     }
+    
+    public void Export(ICanvas Field) {
+        using (FileStream fstream = new FileStream(Path, FileMode.OpenOrCreate)) {
 
-    public void Export(ICanvas field) {
-        using (FileStream fstream = new(Path, FileMode.OpenOrCreate)) {
+            // Настройки сериализации
+            var options = new JsonSerializerOptions {
+                WriteIndented = true, 
+                Converters = { new ShapeConverter() } // Добавляем кастомный конвертер
+            };
+
+          // Сериализация в JSON
+            string json = JsonSerializer.Serialize(Field, options);
+          
+
             string text = "<svg>\n</svg>";
-            if (!File.Exists(Path))
+            if (!File.Exists(Path)) {
                 File.Create(Path);
+            }
             // преобразуем строку в байты
             byte[] buffer = Encoding.Default.GetBytes(text);
             // запись массива байтов в файл
@@ -32,12 +42,13 @@ public class OutputToSVG : IOutput {
         }
     }
 }
-public class OutputToCRNG : IOutput {
+public class OutputToSVG : IOutput {
     public string Path;
-
-    public OutputToCRNG(string path) {
+    public OutputToSVG(string path) {
         Path = path;
     }
-
-    public void Export(ICanvas field) { }
+    public void Export(ICanvas Field) {
+        using (FileStream fstream = new FileStream(Path, FileMode.OpenOrCreate)) { }
+            
+    }
 }
