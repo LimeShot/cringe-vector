6d039c979755483fcb893f9b9c6fa8559c38d050
Nik <nikitasmidt01@gmail.com>
2025-03-10 22:09:47 +0700

    Добавлен буфер команд

---
+++ Client/Model/Commands/CommandBuffer.cs
@@ -0,0 +1,36 @@
+namespace CringeCraft.Client.Model.Commands;
+
+public class CommandsBuffer {
+    private const int CommandsCount = 20;
+    private readonly LinkedList<ICommand> _commands = new LinkedList<ICommand>();
+    private readonly LinkedList<ICommand> _cancelledCommands = new LinkedList<ICommand>();
+
+    public void AddCommand(ICommand command) {
+        if (_commands.Count >= CommandsCount)
+            _commands.RemoveLast();
+
+        _commands.AddFirst(command);
+        _cancelledCommands.Clear();
+    }
+
+
+    public void Undo() {
+        if (_commands.Count > 0) {
+            var removeCommand = _commands.First!.Value;
+            removeCommand.Undo();
+
+            _commands.RemoveFirst();
+            _cancelledCommands.AddFirst(removeCommand);
+        }
+    }
+
+    public void Redo() {
+        if (_cancelledCommands.Count > 0) {
+            var command = _cancelledCommands.First!.Value;
+            command.Redo();
+
+            _cancelledCommands.RemoveFirst();
+            AddCommand(command);
+        }
+    }
+}
