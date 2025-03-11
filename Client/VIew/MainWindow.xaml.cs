aa6024214c5b0efc298bea7b82908622b1d77467
LimeShot <il.chumackov@yandex.ru>
2025-03-11 23:08:13 +0700

    Исправление последнего объединения

---
+++ Client/VIew/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CringeCraft.Client.View;
+
+using System.Windows;
+using OpenTK.Wpf;
+using CringeCraft.Client.ViewModel;
+using System.Windows.Input;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        MainViewModel mViewModel = new(this);
+        DataContext = mViewModel;
+
+        MainWindowElement.Closing += mViewModel.Closing;
+        OpenTkControl.SizeChanged += mViewModel.Resize;
+        OpenTkControl.Ready += mViewModel.InitializeOpenGL;
+        OpenTkControl.Render += mViewModel.Render;
+
+        OpenTkControl.Start(new());
+    }
+}
