3a7020f2546dc0f8e2488c84ac14e2fdead9596d
Paranoint <lantanoint@gmail.com>
2025-03-15 23:18:38 +0700

    Создана кнопочка изменения размера холста

---
+++ Client/View/ResizeCanvasDialog.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Windows;
+
+namespace VectorPaint
+{
+    public partial class ResizeCanvasDialog : Window
+    {
+        public int CanvasWidth { get; private set; }
+        public int CanvasHeight { get; private set; }
+
+        public ResizeCanvasDialog(int currentWidth, int currentHeight)
+        {
+            InitializeComponent();
+            WidthTextBox.Text = currentWidth.ToString();
+            HeightTextBox.Text = currentHeight.ToString();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(WidthTextBox.Text, out int newWidth) &&
+                int.TryParse(HeightTextBox.Text, out int newHeight) &&
+                newWidth > 0 && newHeight > 0)
+            {
+                CanvasWidth = newWidth;
+                CanvasHeight = newHeight;
+                DialogResult = true; // Закрываем окно и возвращаем true
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные размеры!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false; // Закрываем окно без изменений
+        }
+    }
+}
