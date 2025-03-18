e652b2b6bc244bf02593fa58b1fe2f754d0229a6
Nik <nikitasmidt01@gmail.com>
2025-03-18 23:08:59 +0700

    Исправлен DeleteCommand в истории

--- Client/Model/Commands/CommandHistory/CommonCommand/DeleteCommand.cs
+++ Client/Model/Commands/CommandHistory/CommonCommand/DeleteCommand.cs
@@ -6,21 +6,25 @@ namespace CringeCraft.Client.Model.Commands.CommandHistory;
 
 public class DeleteCommand : ICommand {
     private readonly MyCanvas _canvas;
-    private readonly IShape _shape;
+    private readonly List<IShape> _deletedShapes;
 
     public CommandType Type => CommandType.None;
 
 
-    public DeleteCommand(IShape shape, MyCanvas canvas) {
-        _shape = shape;
+    public DeleteCommand(List<IShape> shapes, MyCanvas canvas) {
+        _deletedShapes = shapes;
         _canvas = canvas;
     }
 
     public void Redo() {
-        _canvas.RemoveShape(_shape);
+        foreach (IShape shape in _deletedShapes) {
+            _canvas.RemoveShape(shape);
+        }
     }
 
     public void Undo() {
-        _canvas.AddShape(_shape);
+        foreach (IShape shape in _deletedShapes) {
+            _canvas.AddShape(shape);
+        }
     }
 }
