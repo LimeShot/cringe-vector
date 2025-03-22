846733f5badb7055afe48088958050dc856a7845
LimeShot <il.chumackov@yandex.ru>
2025-03-22 18:17:49 +0700

    Исправлены координаты при приближении

--- Client/Model/Camera/Camera.cs
+++ Client/Model/Camera/Camera.cs
@@ -25,24 +25,21 @@ public class Camera {
    }
 
    public void SetPosition(Vector2 point) {
-      Position += point;
+      Position += point / Zoom;
       UpdateViewMatrix();
    }
 
    public void UndoPosition() {
       Position = (0.0f, 0.0f);
+      Zoom = 1.0f;
       UpdateViewMatrix();
-   }
-
-   public void SetZoom(float zoom) {
-      Zoom = Math.Clamp(zoom, MinZoom, MaxZoom);
    }
 
    public void AdjustZoom(float delta, Vector2 zoomCenter) {
       float oldZoom = Zoom;
       Console.WriteLine(delta);
       float newZoom = Zoom + delta * 0.001f;
-      SetZoom(newZoom);
+      Zoom = Math.Clamp(newZoom, MinZoom, MaxZoom);
 
       if (Math.Abs(newZoom - oldZoom) > float.Epsilon) {
          // Корректируем позицию только при реальном изменении масштаба
@@ -63,15 +60,13 @@ public class Camera {
    }
 
    private void UpdateViewMatrix() {
-      var projection = Matrix4.CreateOrthographicOffCenter(
+      var projection = Matrix4.CreateScale(Zoom, Zoom, 1) * Matrix4.CreateOrthographicOffCenter(
          -_viewportSize.X / 2.0f, _viewportSize.X / 2.0f,
          _viewportSize.Y / 2.0f, -_viewportSize.Y / 2.0f,
          -1, 1);
 
-      var view = Matrix4.CreateScale(Zoom, Zoom, 1) * Matrix4.CreateTranslation(-Position.X, -Position.Y, 0);
+      var view = Matrix4.CreateTranslation(-Position.X, -Position.Y, 0);
 
       MatrixProjecitonPortChanged?.Invoke(projection, view);
    }
-
-
 }
