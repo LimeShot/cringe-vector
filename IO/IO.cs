c469c1a840d8feddef75ccb11442df39023ba8f4
ilyat <ilya.tolo@mail.ru>
2025-03-24 13:28:59 +0700

    Добавлена функция для сохранения в PNG

--- IO/IO.cs
+++ IO/IO.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using CringeCraft.GeometryDash;
 using CringeCraft.GeometryDash.Shape;
 using Microsoft.VisualBasic;
+using SkiaSharp;
 using System.Collections.ObjectModel;
 namespace CringeCraft.IO;
 public interface IExporter {
@@ -29,7 +30,7 @@ public class ExportToCRNG : IExporter {
         }
         // Сериализация в JSON
         string json = JsonConvert.SerializeObject(Field, settings);
-        File.WriteAllText(path, json);        
+        File.WriteAllText(path, json);
     }
 }
 
@@ -50,7 +51,7 @@ public class ImportFromCRNG : IImporter {
         var data = Tuple.Create(shapes, width, height);
 
         return data;
-        
+
     }
 }
 
@@ -58,7 +59,7 @@ public class ImportFromCRNG : IImporter {
 public class ExportToSVG : IExporter {
 
 
-    private string shapeToSVG(IShape shape, float height,int styleNumber) {
+    private string shapeToSVG(IShape shape, float height, int styleNumber) {
         string line;
         switch (shape.GetType().ToString()) {
             case "CringeCraft.GeometryDash.Shape.Line":
@@ -72,11 +73,10 @@ public class ExportToSVG : IExporter {
                 break;
             case "CringeCraft.GeometryDash.Shape.Polygon":
                 line = $"<polygon class=\"st{styleNumber}\"  points=\"";
-                for (int i = 0; i<shape.Nodes.Length;i++)
-                {
-                    line+=$"{shape.Nodes[i].X},{shape.Nodes[i].Y} ";
+                for (int i = 0; i < shape.Nodes.Length; i++) {
+                    line += $"{shape.Nodes[i].X},{shape.Nodes[i].Y} ";
                 }
-                line+=$"\" transform=\"translate({shape.Translate.X}, {shape.Translate.Y * (-1)}) rotate({shape.Rotate})\"/>";
+                line += $"\" transform=\"translate({shape.Translate.X}, {shape.Translate.Y * (-1)}) rotate({shape.Rotate})\"/>";
                 break;
             default:
                 throw new Exception("Это не просто кринж, это не живет в IShape");
@@ -89,31 +89,88 @@ public class ExportToSVG : IExporter {
         using (FileStream fstream = File.Open(path, fileMode)) {
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             string textHead = $"<svg width=\"{Field.Width}\" height=\"{Field.Height}\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"{-Field.Width / 2} {-Field.Height / 2} {Field.Width} {Field.Height}\">\n";
-            string textStyle="";
-            string textShape="";
+            string textStyle = "";
+            string textShape = "";
             List<ShapeStyle> style = new List<ShapeStyle>();
             textStyle += "<defs>\n<style>\n";
             for (int i = 0; i < Field.Shapes.Count; i++) {
                 int j = 0;
                 for (; j < style.Count; j++)
-                    if (style[j]==Field.Shapes[i].Style)
+                    if (style[j] == Field.Shapes[i].Style)
                         break;
-                if (j == style.Count){
+                if (j == style.Count) {
                     if (Field.Shapes[i].Style.Fill)
-                        textStyle+=$".st{j} {{\nstroke: rgb({Field.Shapes[i].Style.ColorOutline.X*255}, {Field.Shapes[i].Style.ColorOutline.Y*255}, {Field.Shapes[i].Style.ColorOutline.Z*255});\nfill: rgb({Field.Shapes[i].Style.ColorFill.X*255}, {Field.Shapes[i].Style.ColorFill.Y*255}, {Field.Shapes[i].Style.ColorFill.Z*255});\n}}\n";
+                        textStyle += $".st{j} {{\nstroke: rgb({Field.Shapes[i].Style.ColorOutline.X * 255}, {Field.Shapes[i].Style.ColorOutline.Y * 255}, {Field.Shapes[i].Style.ColorOutline.Z * 255});\nfill: rgb({Field.Shapes[i].Style.ColorFill.X * 255}, {Field.Shapes[i].Style.ColorFill.Y * 255}, {Field.Shapes[i].Style.ColorFill.Z * 255});\n}}\n";
                     else
-                        textStyle+=$".st{j} {{\nstroke: rgb({Field.Shapes[i].Style.ColorOutline.X*255}, {Field.Shapes[i].Style.ColorOutline.Y*255}, {Field.Shapes[i].Style.ColorOutline.Z*255});\nfill: none;\n}}\n";
+                        textStyle += $".st{j} {{\nstroke: rgb({Field.Shapes[i].Style.ColorOutline.X * 255}, {Field.Shapes[i].Style.ColorOutline.Y * 255}, {Field.Shapes[i].Style.ColorOutline.Z * 255});\nfill: none;\n}}\n";
                     style.Add(Field.Shapes[i].Style);
-                    }
-                textShape += $"{shapeToSVG(Field.Shapes[i], Field.Height,j)}\n";// передем номер стиля
+                }
+                textShape += $"{shapeToSVG(Field.Shapes[i], Field.Height, j)}\n";// передем номер стиля
             }
-            textStyle+="</style>\n</defs>\n";
+            textStyle += "</style>\n</defs>\n";
             textShape += "</svg>";
-            byte[] buffer = Encoding.Default.GetBytes(textHead+textStyle+textShape);
+            byte[] buffer = Encoding.Default.GetBytes(textHead + textStyle + textShape);
             fstream.Flush();
             fstream.WriteAsync(buffer, 0, buffer.Length);
 
         }
+
+    }
+}
+
+public class ExportToPNG : IExporter {
+    public void Export(string path, ICanvas Field) {
+
+        int lastSlashIndex = path.LastIndexOf('/');
+        string pathWithoutFileName = "";
+
+        try {
+            if (lastSlashIndex < 0) {
+                throw new InvalidOperationException("Символ '/' не найден в пути.");
+            }
+            pathWithoutFileName = path.Substring(0, lastSlashIndex);
+        } catch (InvalidOperationException ex) {
+            Console.WriteLine(ex.Message);
+        }
+        // Временный файл для .svg
+        string tempFilePath = pathWithoutFileName + Path.GetTempFileName();
+        string svgFilePath = Path.ChangeExtension(tempFilePath, ".svg");
+
+        var tmpExp = new ExportToSVG();
+        tmpExp.Export(svgFilePath, Field);
+
+        // Загрузка SVG файла
+        var svg = new SkiaSharp.Extended.Svg.SKSvg();
+        svg.Load(svgFilePath);
+
+        // Определение размеров изображения
+        int width = (int)Field.Width; // Ширина PNG
+        int height = (int)Field.Height; // Высота PNG
+
+        // Создание растрового изображения
+        var bitmap = new SKBitmap(width, height);
+        var canvas = new SKCanvas(bitmap);
+
+        // Очистка холста (белый фон)
+        canvas.Clear(SKColors.White);
+
+        // Масштабирование SVG до размеров PNG
+        var matrix = SKMatrix.CreateScale((float)width / svg.Picture.CullRect.Width, (float)height / svg.Picture.CullRect.Height);
+        canvas.SetMatrix(matrix);
+
+        // Отрисовка SVG на холсте
+        canvas.DrawPicture(svg.Picture);
+        canvas.Flush();
+
+        // Сохранение растрового изображения в PNG файл
+        using (var image = SKImage.FromBitmap(bitmap))
+        using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+        using (var stream = System.IO.File.OpenWrite(path)) {
+            data.SaveTo(stream);
+        }
+
+        //Console.WriteLine("SVG успешно конвертирован в PNG.");
+        File.Delete(tempFilePath);
 
     }
 }
