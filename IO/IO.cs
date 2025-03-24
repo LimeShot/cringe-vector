624db90b62c057b73a2176a74e01c7987b9d01d4
ilyat <ilya.tolo@mail.ru>
2025-03-24 12:00:34 +0700

    Создана функция сохранения в PNG

--- IO/IO.cs
+++ IO/IO.cs
@@ -5,6 +5,8 @@ using CringeCraft.GeometryDash;
 using CringeCraft.GeometryDash.Shape;
 using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using SkiaSharp;
+//using SkiaSharp.Svg;
 namespace CringeCraft.IO;
 public interface IExporter {
     public void Export(string path, ICanvas Field); // функция, берущая класс с фигурами
@@ -29,7 +31,7 @@ public class ExportToCRNG : IExporter {
         }
         // Сериализация в JSON
         string json = JsonConvert.SerializeObject(Field, settings);
-        File.WriteAllText(path, json);        
+        File.WriteAllText(path, json);
     }
 }
 
@@ -51,7 +53,7 @@ public class ImportFromCRNG : IImporter {
         Console.WriteLine(width);
         Console.WriteLine(shapes.Count);
         return data;
-        
+
     }
 }
 
@@ -96,3 +98,88 @@ public class ExportToSVG : IExporter {
 
     }
 }
+
+public class ExportToPNG : IExporter {
+    public void Export(string path, ICanvas Field) {
+
+        int lastSlashIndex = path.LastIndexOf('/');
+        string pathWithoutFileName = "";
+
+        try {
+            if (lastSlashIndex < 0) {
+                throw new InvalidOperationException("Символ '/' не найден в пути.");
+            }
+            pathWithoutFileName = path.Substring(0, lastSlashIndex);
+        } catch (InvalidOperationException ex) {
+            Console.WriteLine(ex.Message);
+        }
+        // Временный файл для .svg
+        string tempFilePath = pathWithoutFileName + Path.GetTempFileName();
+        string svgFilePath = Path.ChangeExtension(tempFilePath, ".svg");
+
+        var tmpExp = new ExportToSVG();
+        tmpExp.Export(svgFilePath, Field);
+
+        // Загрузка SVG файла
+        var svg = new SkiaSharp.Extended.Svg.SKSvg();
+        svg.Load(svgFilePath);
+
+        // Определение размеров изображения
+        int width = (int)Field.Width; // Ширина PNG
+        int height = (int)Field.Height; // Высота PNG
+
+        // Создание растрового изображения
+        var bitmap = new SKBitmap(width, height);
+        var canvas = new SKCanvas(bitmap);
+
+        // Очистка холста (белый фон)
+        canvas.Clear(SKColors.White);
+
+        // Масштабирование SVG до размеров PNG
+        var matrix = SKMatrix.CreateScale((float)width / svg.Picture.CullRect.Width, (float)height / svg.Picture.CullRect.Height);
+        canvas.SetMatrix(matrix);
+
+        // Отрисовка SVG на холсте
+        canvas.DrawPicture(svg.Picture);
+        canvas.Flush();
+
+        // Сохранение растрового изображения в PNG файл
+        using (var image = SKImage.FromBitmap(bitmap))
+        using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+        using (var stream = System.IO.File.OpenWrite(path)) {
+            data.SaveTo(stream);
+        }
+
+        //Console.WriteLine("SVG успешно конвертирован в PNG.");
+        File.Delete(tempFilePath);
+
+    }
+}
+/*
+public class ExportToPNG : IExporter {
+    public void Export(string path, ICanvas Field) {
+        byte[] pixels = new byte[(int)Field.Width * (int)Field.Height * 4]; // RGBA
+
+        // Читаем пиксели из буфера кадров
+        
+        GL.ReadPixels(0, 0, (int)Field.Width, (int)Field.Height, PixelFormat.Rgba, PixelType.UnsignedByte, pixels);
+
+        // Переворачиваем изображение по вертикали (OpenGL хранит данные снизу вверх)
+        byte[] flippedPixels = new byte[(int)Field.Width * (int)Field.Height * 4];
+        for (int y = 0; y < (int)Field.Height; y++) {
+            int srcOffset = y * (int)Field.Width * 4;
+            int dstOffset = ((int)Field.Height - y - 1) * (int)Field.Width * 4;
+            Array.Copy(pixels, srcOffset, flippedPixels, dstOffset, (int)Field.Width * 4);
+        }
+
+        // Сохраняем изображение в PNG с помощью System.Drawing
+        using (Bitmap bitmap = new Bitmap((int)Field.Width, (int)Field.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb)) {
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, (int)Field.Width, (int)Field.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+            System.Runtime.InteropServices.Marshal.Copy(flippedPixels, 0, bitmapData.Scan0, flippedPixels.Length);
+            bitmap.UnlockBits(bitmapData);
+
+            bitmap.Save(path, ImageFormat.Png);
+        }
+    }
+}
+*/
